2746c26ed0716a19e5b8641ab9da46e8ac8d4dbb
monikadembek <monika.dembek@gmail.com>
2018-05-18 00:13:33 +0200

    coded about section and added links to github and ln on front page

--- js/modalWindowTransition.js
+++ js/modalWindowTransition.js
@@ -1,4 +1,9 @@
 var triggerBtn = document.querySelector('.modal-trigger');
+var btnReadMore = document.querySelector('.btn-more');
+var btnReadLess = document.querySelector('.btn-less');
+var aboutFirst = document.querySelector('.modal-about--first-page');
+var aboutSecond = document.querySelector('.modal-about--second-page');
+
 
 
 triggerBtn.addEventListener('click', function(e){
@@ -14,4 +19,19 @@ var btnClose = document.querySelector('.modal__close');
 btnClose.addEventListener('click', function() {
 	var openedModal = document.querySelector('.modal-show');
 	openedModal.classList.remove('modal-show');
+});
+
+
+
+
+
+btnReadMore.addEventListener('click', function(){
+	aboutFirst.classList.toggle('hide');
+	aboutSecond.classList.toggle('hide');
+});
+
+
+btnReadLess.addEventListener('click', function(){
+	aboutFirst.classList.toggle('hide');
+	aboutSecond.classList.toggle('hide');
 });
