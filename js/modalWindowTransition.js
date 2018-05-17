7c13b7f337104b127f81575e4e63761317d81230
monikadembek <monika.dembek@gmail.com>
2018-05-18 01:34:58 +0200

    coded portfolio section

--- js/modalWindowTransition.js
+++ js/modalWindowTransition.js
@@ -1,37 +1,41 @@
-var triggerBtn = document.querySelector('.modal-trigger');
+var triggerBtn = document.querySelectorAll('.modal-trigger');
 var btnReadMore = document.querySelector('.btn-more');
 var btnReadLess = document.querySelector('.btn-less');
 var aboutFirst = document.querySelector('.modal-about--first-page');
 var aboutSecond = document.querySelector('.modal-about--second-page');
 
 
+for (var i = 0; i < triggerBtn.length; i++) {
+	triggerBtn[i].addEventListener('click', function(e){
+		e.preventDefault();
+		var id = this.dataset.modal; 
+		var modal = document.getElementById(id);
+		modal.classList.add('modal-show');
+	});	
+}
 
-triggerBtn.addEventListener('click', function(e){
-	e.preventDefault();
-	var id = this.dataset.modal; 
-	var modal = document.getElementById(id);
-	modal.classList.add('modal-show');
-});
 
 
-var btnClose = document.querySelector('.modal__close');
+var btnClose = document.querySelectorAll('.modal__close');
 
-btnClose.addEventListener('click', function() {
-	var openedModal = document.querySelector('.modal-show');
-	openedModal.classList.remove('modal-show');
-});
+for (var i = 0; i < btnClose.length; i++) {
+	btnClose[i].addEventListener('click', function() {
+		var openedModal = document.querySelector('.modal-show');
+		openedModal.classList.remove('modal-show');
+	});
+}
 
 
 
 
 
 btnReadMore.addEventListener('click', function(){
-	aboutFirst.classList.toggle('hide');
-	aboutSecond.classList.toggle('hide');
+	aboutFirst.classList.toggle('u-hide');
+	aboutSecond.classList.toggle('u-hide');
 });
 
 
 btnReadLess.addEventListener('click', function(){
-	aboutFirst.classList.toggle('hide');
-	aboutSecond.classList.toggle('hide');
+	aboutFirst.classList.remove('u-hide');
+	aboutSecond.classList.add('u-hide');
 });
